41605602b8285983c8fa8a06cf919845dfe872d2
Crowdbotics <team@crowdbotics.com>
2021-09-16 20:08:47 +0000

    Update models

--- backend/withered_rain_30622/wsgi.py
+++ backend/withered_rain_30622/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'withered_rain_30622.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "withered_rain_30622.settings")
 
 application = get_wsgi_application()
