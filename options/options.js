256200690e14fac3108f10f0589363c187a44a5f
Crowdbotics <team@crowdbotics.com>
2021-09-16 19:39:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_rain_30622Identifier",
+  url: "https://withered_rain_30622Identifier.botics.co",
+  api: "https://withered_rain_30622Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
